470dc8f9a84482a71bc80a719460c6e42f263ea4
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-07-13 19:07:04 +0300

    function created to render fourth page

--- src/dom-component.js
+++ src/dom-component.js
@@ -1,7 +1,7 @@
 import data from "./data.json";
 //function to draw homepage content
 function homePageSection(ele){
-  const template = ` 
+    const template = ` 
   <section class="home-page-section">
     <div class="intro-text">SO YOU WANT TO TRAVEL TO</div>
     <h1 class="intro-title">SPACE</h1>
@@ -20,27 +20,27 @@ function homePageSection(ele){
     </div>
     </div>
 `;
-ele.textContent = '';
-ele.innerHTML = template;
+    ele.textContent = '';
+    ele.innerHTML = template;
 }
 //function choose image from json based on the screen width 
 function chooseImage(ele,number){
-  const mobileScreen = window.matchMedia("(max-width: 500px)");
-  const largeScreen = window.matchMedia("(min-width:600px)");
-  if(largeScreen.matches === true){
-    const image = require(`${data.destinations[number].images.png}`);
-    ele.style.backgroundImage = `url('${image}')`;
-  }
-  else if(mobileScreen.matches === true){
-    const image = require(`${data.destinations[number].images.webp}`);
-    ele.style.backgroundImage = `url('${image}')`;
-  }
+    const mobileScreen = window.matchMedia("(max-width: 500px)");
+    const largeScreen = window.matchMedia("(min-width:600px)");
+    if(largeScreen.matches === true){
+        const image = require(`${data.destinations[number].images.png}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
+    else if(mobileScreen.matches === true){
+        const image = require(`${data.destinations[number].images.webp}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
   
 }
   
 //function destination page (second page)
 function secondPage(ele){
-  const template = `
+    const template = `
   <section class="planet-info-holder">
    <section class="current-page-info">
     <div class="current-page">
@@ -65,12 +65,12 @@ function secondPage(ele){
 
 </section>
  `;
- ele.textContent = "";
- ele.innerHTML = template;
+    ele.textContent = "";
+    ele.innerHTML = template;
 }
 
 function destinationPlanet(ele,number){
-  const template = ` 
+    const template = ` 
   <h1 class="planet-name">${data.destinations[number].name}</h1>
   <p class="planet-par">${data.destinations[number].description}</p>
   <hr>
@@ -85,56 +85,108 @@ function destinationPlanet(ele,number){
     </div>
   </div>
   `;
-  ele.textContent = '';
-  ele.innerHTML = template;
+    ele.textContent = '';
+    ele.innerHTML = template;
 }
 function thirdPage(ele){
-  const template = ` 
-  <section class="crew-image-holder">
-     <div class="crew-image" data-crew-image></div>
-  </section>
+    const template = ` 
+ 
   <section class="crew-info-holder">
      <section class="current-page-info">
       <div class="current-page">
        <span class="link-number" aria-hidden="true">02</span>
        <div class="link-name">MEET YOUR CREW</div>
       </div>
+      <section class="crew-image-holder">
+      <div class="crew-image" data-crew-image></div>
+   </section>
+
      </section>
      <div class="person-info">
        <article class="person-detail-info" data-crew-bio></article>
-       <div class="slider-btn-holder">
-       <button aria-label="crew-member" class="slider-btn"  data-crew="0"></button>
-       <button aria-label="crew-member" class="slider-btn"  data-crew="1"></button>
-       <button aria-label="crew-member" class="slider-btn"  data-crew="2"></button>
-       <button aria-label="crew-member" class="slider-btn"  data-crew="3"></button>
-       
+       <div class="slider-btn-holder" data-slider-btn>
+        <button aria-label="crew-member" class="slider-btn"  data-crew="0" ></button>
+        <button aria-label="crew-member" class="slider-btn"  data-crew="1" ></button>
+        <button aria-label="crew-member" class="slider-btn"  data-crew="2" ></button>
+        <button aria-label="crew-member" class="slider-btn"  data-crew="3"></button>
       </div>
      </div>
 
    </section>`;
-   ele.innerHTML = template;
+    ele.innerHTML = template;
 }
 function personInfo(ele,number){
-  const bioTemplate = `
+    const bioTemplate = `
     <div class="person-job">${data.crew[number].role}</div>
     <h1 class="crew-name">${data.crew[number].name}</h1>
     <p class="crew-bio" data-crew-info>${data.crew[number].bio}</p>
   `;
-  ele.innerHTML = bioTemplate;
+    ele.innerHTML = bioTemplate;
 }
 function personImage(ele,number){
-  const mobileScreen = window.matchMedia("(max-width: 500px)");
-  const largeScreen = window.matchMedia("(min-width:600px)");
-  if(largeScreen.matches === true){
-    const image = require(`${data.crew[number].images.png}`);
-     ele.style.backgroundImage = `url('${image}')`;
-  }
-  else if(mobileScreen.matches === true){
-    const image = require(`${data.crew[number].images.webp}`);
-    ele.style.backgroundImage = `url('${image}')`;
-  }
+    const mobileScreen = window.matchMedia("(max-width: 500px)");
+    const largeScreen = window.matchMedia("(min-width:600px)");
+    if(largeScreen.matches === true){
+        const image = require(`${data.crew[number].images.png}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
+    else if(mobileScreen.matches === true){
+        const image = require(`${data.crew[number].images.webp}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
+}
+//function to draw fourth page
+function fourthPage(ele){
+    const template = `  
+    <section class="current-page-info">
+    <div class="current-page">
+    <span class="link-number" aria-hidden="true">02</span>
+    <div class="link-name">SPACE LAUNCH 101</div>
+    </div>
+    <div class="techno-image-holder" data-techno-holder>
+      <div class="techno-image" data-techno-image></div>
+    </div>
+  </section>
+    
+    <section class="technology-info-holder">
+   
+   
+     <nav class="techno-btn-holder">
+        <button  aria-label="technology-used" class="techno-btn" data-technology="0">1</button>
+        <button  aria-label="technology-used" class="techno-btn" data-technology="1">2</button>
+        <button  aria-label="technology-used" class="techno-btn" data-technology="2">3</button>
+      </nav>
+     <div class="title">THE TECHNOLOGY...</div>
+      <article class="techno-par" data-techno-info></article>
+   
+    </section>`;
+    ele.textContent = '';
+    ele.innerHTML = template;
+}
+//function to pick image for technology page based on screen
+function chooseTechnoImage(ele,number){
+    const mobileScreen = window.matchMedia("(max-width: 500px)");
+    const largeScreen = window.matchMedia("(min-width:600px)");
+    if(largeScreen.matches === true){
+        const image = require(`${data.technology[number].images.portrait}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
+    else if(mobileScreen.matches === true){
+        const image = require(`${data.technology[number].images.landscape}`);
+        ele.style.backgroundImage = `url('${image}')`;
+    }
 
+}
+//function to update paragraph about the technology used
+function technoInfo(ele,number){
+    const template = ` 
+    <h1 class="techno-name">${data.technology[number].name}</h1>
+    <p class="techno-info">${data.technology[number].description}</p>
+    `;
+    ele.textContent = '';
+    ele.innerHTML = template;
 }
 
 
-export {homePageSection,secondPage,destinationPlanet,chooseImage,thirdPage,personInfo,personImage};
+export {homePageSection,secondPage,destinationPlanet,chooseImage,thirdPage,personInfo,
+    personImage,fourthPage,chooseTechnoImage,technoInfo};
