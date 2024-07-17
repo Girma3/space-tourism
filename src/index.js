92ac9f1aca4219bb66cb8d147fbad6bee883f46c
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-07-17 09:21:36 +0300

    focus style for crew changed

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import "./desktop.css";
 import { homePageSection,secondPage,destinationPlanet,chooseImage, 
     thirdPage,personInfo,personImage,
     fourthPage,technoInfo,chooseTechnoImage,
-    chooseBgImage
+    chooseBgImage,navBtnFocus,sliderBtnFocus,tabFocus
 } from "./dom-component";
-
 const pageNav = document.querySelector("[data-page-nav]");
 const main = document.querySelector('main');
 const openMenu = document.querySelector("[data-open-menu]");
@@ -15,6 +14,7 @@ const body = document.body;
 const largeScreen = window.matchMedia("(min-width:501px)");
 chooseBgImage(body,0);
 homePageSection(main);
+navBtnFocus(0);
 pageNav.addEventListener("click",e=>{
     if(e.target.matches("[data-home-btn]")){
         homePageSection(main);
@@ -25,6 +25,7 @@ pageNav.addEventListener("click",e=>{
             openMenu.style.display = "block";
             closeMenu.style.display = "none";
         }
+        navBtnFocus(0);
     }
     else if(e.target.matches("[data-destination-btn]")){
         secondPage(main);
@@ -41,40 +42,40 @@ pageNav.addEventListener("click",e=>{
             openMenu.style.display = "block";
             closeMenu.style.display = "none";
         }
+        navBtnFocus(1);
     }
     else if(e.target.matches("[data-crew-btn]")){
         thirdPage(main);
         const crewArticle = document.querySelector("[data-crew-bio]");
         const imageHolder = document.querySelector("[data-crew-image]");
-        const slideBtn = document.querySelector(`[data-crew="3"]`);
         personInfo(crewArticle,3);
-        slideBtn.focus();
         personImage(imageHolder,3);
         chooseBgImage(body,2);
+        sliderBtnFocus(3);
         //for mobile
         if(largeScreen.matches === false){
             nav.setAttribute("data-nav-links","false");
             openMenu.style.display = "block";
             closeMenu.style.display = "none";
         }
+        navBtnFocus(2);
     }
     else if(e.target.matches("[data-technology-btn]")){
         fourthPage(main);
         const techArticle = document.querySelector("[data-techno-info]");
         const imageHolder = document.querySelector("[data-techno-image]");
-        const technoTab = document.querySelector(`[data-technology="0"]`);
         technoInfo(techArticle,0);
-        technoTab.focus();
         chooseTechnoImage(imageHolder,0);
         chooseBgImage(body,3);
+        tabFocus(0);
         //for mobile hide menu
         if(largeScreen.matches === false){
             nav.setAttribute("data-nav-links","false");
             openMenu.style.display = "block";
             closeMenu.style.display = "none";
         }
+        navBtnFocus(3);
     }
-    
 });
 main.addEventListener('click',(e)=>{
     if(e.target.matches("[data-explore-btn]")){
@@ -85,6 +86,7 @@ main.addEventListener('click',(e)=>{
         btn.focus();
         chooseImage(planetImage,0);
         destinationPlanet(article,0);
+        navBtnFocus(0);
     }
     //second page events
     else if(e.target.matches(`[data-nav="0"]`)){
@@ -120,7 +122,7 @@ main.addEventListener('click',(e)=>{
         personInfo(crewArticle,0);
         slideBtn.focus();
         personImage(imageHolder,0);
-
+        sliderBtnFocus(0);
     }
     else if(e.target.matches(`[data-crew="1"]`)){
         const crewArticle = document.querySelector("[data-crew-bio]");
@@ -129,6 +131,7 @@ main.addEventListener('click',(e)=>{
         personInfo(crewArticle,1);
         slideBtn.focus();
         personImage(imageHolder,1);
+        sliderBtnFocus(1);
     }
     else if(e.target.matches(`[data-crew="2"]`)){
         const crewArticle = document.querySelector("[data-crew-bio]");
@@ -137,50 +140,46 @@ main.addEventListener('click',(e)=>{
         personInfo(crewArticle,2);
         slideBtn.focus();
         personImage(imageHolder,2);
+        sliderBtnFocus(2);
     }
     else if(e.target.matches(`[data-crew="3"]`)){
-        console.log("hey");
         const crewArticle = document.querySelector("[data-crew-bio]");
         const imageHolder = document.querySelector("[data-crew-image]");
         const slideBtn = document.querySelector(`[data-crew="3"]`);
         personInfo(crewArticle,3);
         slideBtn.focus();
         personImage(imageHolder,3);
+        sliderBtnFocus(3);
     }
     //fourth page event
     else if(e.target.matches(`[data-technology="0"]`)){
         const techArticle = document.querySelector("[data-techno-info]");
         const imageHolder = document.querySelector("[data-techno-image]");
-        const technoTab = document.querySelector(`[data-technology="0"]`);
         technoInfo(techArticle,0);
-        technoTab.focus();
         chooseTechnoImage(imageHolder,0);
+        tabFocus(0);
     }
     else if(e.target.matches(`[data-technology="1"]`)){
         const techArticle = document.querySelector("[data-techno-info]");
         const imageHolder = document.querySelector("[data-techno-image]");
-        const technoTab = document.querySelector(`[data-technology="1"]`);
         technoInfo(techArticle,1);
-        technoTab.focus();
         chooseTechnoImage(imageHolder,1);
+        tabFocus(1);
     }
     else if(e.target.matches(`[data-technology="2"]`)){
         const techArticle = document.querySelector("[data-techno-info]");
         const imageHolder = document.querySelector("[data-techno-image]");
-        const technoTab = document.querySelector(`[data-technology="2"]`);
         technoInfo(techArticle,2);
-        technoTab.focus();
         chooseTechnoImage(imageHolder,2);
+        tabFocus(2);
     }
 });
-
 openMenu.addEventListener('click',()=>{
     if(nav.getAttribute("data-nav-links") === "false"){
         nav.setAttribute("data-nav-links","true");
         closeMenu.style.display = "block";
         openMenu.style.display ="none";
     }
-
 });
 closeMenu.addEventListener('click',()=>{
     if(nav.getAttribute("data-nav-links") === "true"){
@@ -189,24 +188,21 @@ closeMenu.addEventListener('click',()=>{
         closeMenu.style.display = "none";
     }
 });
-console.log("hey there fam");
-
-
-
-
 largeScreen.addEventListener('change',()=>{
-    //nav.setAttribute("data-nav-links","false");
-    console.log(largeScreen);
+    //remove mobile hover and focus style
+    const ul = document.querySelector('.home-page-nav').children; 
+    const li = [...ul];
     if(largeScreen.matches ===true){
         nav.setAttribute("data-nav-links","true");
         closeMenu.style.display = 'none';   
         openMenu.style.display = 'none';
+        li.forEach(nav=>{
+            nav.style.borderRight= "none";
+        });
     }
     else if(largeScreen.matches ===false){
-        console.log(largeScreen,"e");
         nav.setAttribute("data-nav-links","false");
         openMenu.style.display = "block";
-       
     }
 
 });
