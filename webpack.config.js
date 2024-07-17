61927534c7b772b677052405d0f0e90b6567b9ad
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2024-07-17 09:22:44 +0300

    file loader removed

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,54 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-
-  entry: {
-    index: "./src/index.js",
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: "space-tourism",
-      //favicon: "./src/assets/fonts/favicon.ico",
-      template: "./src/template.html",
-      chunks: ["index"],
-      filename: "index.html",
-    }),
-  ],
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-
-      {
-        test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
-          },
-        },
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+    mode: "development",
+    entry: {
+        index: "./src/index.js",
+    },
+    devtool: "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: "space-tourism",
+            favicon: "./src/assets/fonts/favicon-32x32.png",
+            template: "./src/template.html",
+            chunks: ["index"],
+            filename: "index.html",
+        }),
     ],
-  },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [["@babel/preset-env", { targets: "defaults" }]],
+                    },
+                },
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
 };
